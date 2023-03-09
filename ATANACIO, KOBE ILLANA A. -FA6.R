1c3eb12763ed43214e1ae94dd316356319da9c27
NorthstarKIA <127362468+NorthstarKIA@users.noreply.github.com>
2023-03-09 09:16:11 +0800

    Update ATANACIO, KOBE ILLANA A. -FA6.R

--- ATANACIO, KOBE ILLANA A. -FA6.R
+++ ATANACIO, KOBE ILLANA A. -FA6.R
@@ -7,10 +7,14 @@ n
 U <- runif(n)
 # Generating numbers from Geometric distribution 
 X <- ceiling(log(U)/log(q))
+# Calculating and Printing of mean
 mean_x <- mean(X)
 format(round(mean_x, 2), nsmall = 2)
+# Calculating and Printing of variance
 var_x <- var(X)
 format(round(var_x, 2), nsmall = 2)
+# Calculating and Printing of standard deviation
 sd_x <- sd(X)
 format(round(sd_x, 2), nsmall = 2)
+# Histogram of X
 hist(X,freq = FALSE)
