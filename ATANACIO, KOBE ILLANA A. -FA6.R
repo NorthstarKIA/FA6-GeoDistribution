468d086e44544cea0043962edebcb6105f8cc48b
NorthstarKIA <127362468+NorthstarKIA@users.noreply.github.com>
2023-03-09 10:25:48 +0800

    Update ATANACIO, KOBE ILLANA A. -FA6.R

--- ATANACIO, KOBE ILLANA A. -FA6.R
+++ ATANACIO, KOBE ILLANA A. -FA6.R
@@ -7,15 +7,19 @@ n
 U <- runif(n)
 # Generating numbers from Geometric distribution 
 rgeom(n,p)
-GeoDist <- rgeom(n,p)
+x <- rgeom(n,p)
 # Calculating and Printing of mean
-mean_x <- mean(rgeom(n,p))
+mean_x <- round(mean(x), 2)
 format(round(mean_x, 2), nsmall = 2)
+cat("Mean:", mean_x)
 # Calculating and Printing of variance
-var_x <- var(rgeom(n,p))
+var_x <- round(var(x), 2)
 format(round(var_x, 2), nsmall = 2)
+cat("Variance:", var_x)
 # Calculating and Printing of standard deviation
-sd_x <- sd(rgeom(n,p))
+sd_x <- round(sd(x), 2)
 format(round(sd_x, 2), nsmall = 2)
+cat("Standard Deviation:", sd_x)
 # Histogram of Geometric Distribution
-hist(GeoDist,freq = FALSE)
+hist(x, breaks = seq(min(x), max(x)+1, by = 1), main = "Geometric Distribution Histogram", xlab = "Trials for success")
+
